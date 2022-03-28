2f656d54e07c26f6fc87cddfeef147e451eeeebf
cesar mejia <cemejiab@correo.usbcali.edu.co>
2022-03-28 08:12:12 -0500

    2.9

---
+++ programs.sh
@@ -0,0 +1,4 @@
+sudo pacman -S picom pulseaudio pavucontrol xorg-xrandr ranger htop
+sudo pacman -S gnome-disk-utility neofetch
+sudo pacman -S nvidia nvidia-utils nvidia-settings vulkan-icd-loader nvidia-dkms
+sudo pacman -S lib32-mesa vulkan-intel lib32-vulkan-intel vulkan-icd-loader lib32-vulkan-icd-loader
